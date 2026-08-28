84ec39b25160cbb43a471a7ce345cd497d93a6bb
Senthalir viveka <senthalirviv@gmail.com>
2026-08-28 21:33:02 +0530

    Clean up comments in main.java
    Removed comments from the main class in main.java.

--- First project/main.java
+++ First project/main.java
@@ -1,10 +1,7 @@
 
 
 public class main{
-     //first java program
-     /*hey lol
-     cool
-     */
+
     public static void main(String[] args){
         System.out.println("I love One direction");
         System.out.print("Vampire diaries\n");
